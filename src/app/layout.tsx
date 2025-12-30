f20f18e4703f00f9c8f383348c8d3f1f1c51099f
bongnteh-romarick-ndzelen <ndzelenromarick@gmail.com>
2025-12-30 06:05:12 -0800

    fixing the usesearchparams error in the client side

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
   return (
     <html lang="en" className={cn(inter.variable, 'dark')} suppressHydrationWarning>
       <head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.png" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </head>
       <body className={cn(
